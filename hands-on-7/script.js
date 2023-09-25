cf5271a94b28c1d658d73d8830de45ea0705bd19
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-09-25 14:44:46 +0700

    solution hands-on 7 phase 2

--- hands-on-7/script.js
+++ hands-on-7/script.js
@@ -20,12 +20,36 @@ const login = async (username, password) => {
   }
 }
 
+const getMyInformation = async () => {
+  const token = localStorage.getItem('token')
+
+  try {
+    const res = await fetch('https://api.learnhub.thanayut.in.th/auth/me', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const data = await res.json()
+
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 const main = () => {
   const usernameInput = document.getElementById('username')
 
   const passwordInput = document.getElementById('password')
 
   const submitButton = document.getElementById('submit')
+
+  const getMyInfoButton = document.getElementById('get-info')
+
+  getMyInfoButton.addEventListener('click', getMyInformation)
 
   submitButton.addEventListener('click', async (e) => {
     e.preventDefault()
